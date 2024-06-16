5becbe19af83ceb51688bfcd403b0774a836be54
Alexandre Foulc <alexandre-foulc@orange.fr>
2024-06-16 15:06:56 +0200

    Désactivation des cookies/ pas utiles pour le moment

--- public/index.php
+++ public/index.php
@@ -12,15 +12,17 @@ require_once '../config/config.php';
 
 /** 
  * Sécurise le cookie de session pour prévenir certaines attaques (comme le vol de cookies via des scripts côté client (XSS) ou via des connexions non sécurisées).
+ * Je commente car je ne vois pas l'intérêt de le mettre en place pour le moment.
+ * Le cookie de Session dans lequel je place les infos de mon User connecté est déjà sécurisé par défaut car il est stocké sur le serveur et non sur le navigateur.
  */
-session_set_cookie_params([
-  'lifetime' => 3600, // durée de vie du cookie de session 1h
-  'path' => '/',
-  'domain' => $_SERVER['SERVER_NAME'],
-  //'secure' => true, // pas activé car pas de HTTPS sur le serveur de développement
-  'httponly' => true,
-  'samesite' => 'Strict', // empêche le cookie d'être envoyé avec des requêtes provenant de sites tiers
-]);
+// session_set_cookie_params([
+//   'lifetime' => 3600, // (exe: durée de vie du cookie de session 1h)
+//   'path' => '/',
+//   'domain' => $_SERVER['SERVER_NAME'],
+//   //'secure' => true, // pas activé car pas de HTTPS sur le serveur de développement
+//   'httponly' => true,
+//   'samesite' => 'Strict', // empêchera le cookie d'être envoyé avec des requêtes provenant de sites tiers
+// ]);
 
 
 // Démarrage de la session
